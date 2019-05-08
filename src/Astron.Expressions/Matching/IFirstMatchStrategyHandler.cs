d87c89d780c0e1a4e490b1afe32d2826eda628cd
thenameless314159 <valentin.esslinger@gmail.com>
2019-05-08 21:36:25 +0200

    created first match strategy handler interface

---
+++ src/Astron.Expressions/Matching/IFirstMatchStrategyHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Astron.Expressions.Specifications;
+
+namespace Astron.Expressions.Matching
+{
+    public interface IFirstMatchStrategyHandler<TDel, in TComp> : IMatchingHandler<ISpecification<PropertyInfo>, TComp>
+        where TDel : Delegate
+        where TComp : IExpressionCompiler<TDel>
+    {
+    }
+}
